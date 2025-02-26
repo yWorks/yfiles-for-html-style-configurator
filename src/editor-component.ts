1677ced7785d858cad7bd85356cc78727ef1f223
Lukas Hollaender <lukas.hollaender@yworks.com>
2025-02-26 11:37:04 +0100

    integrate codemirror

--- src/editor-component.ts
+++ src/editor-component.ts
@@ -1,19 +1,41 @@
 import { StyleConfiguration } from './style-configuration'
+import { basicSetup } from 'codemirror'
+import { EditorView } from '@codemirror/view'
 
 export class EditorComponent {
-  private textArea: HTMLTextAreaElement
   private copyButton: HTMLButtonElement
+  private view: EditorView
 
-  constructor() {
-    this.textArea = document.querySelector('.editor') as HTMLTextAreaElement
+  constructor(parent: Element | DocumentFragment) {
     this.copyButton = document.querySelector('.copy-code') as HTMLButtonElement
 
     this.copyButton.addEventListener('click', () => {
-      void navigator.clipboard.writeText(this.textArea.value)
+      void navigator.clipboard.writeText(this.getText())
+    })
+
+    this.view = new EditorView({
+      doc: '',
+      parent,
+      extensions: [basicSetup],
     })
   }
 
   update(json: StyleConfiguration) {
-    this.textArea.value = `new ShapeNodeStyle(${JSON.stringify(json, null, 2)})`
+    const text = `new ShapeNodeStyle(${JSON.stringify(json, null, 2)})`
+    this.setText(text)
+  }
+
+  private getText() {
+    return this.view.state.doc.toString()
+  }
+
+  private setText(text: string) {
+    this.view.dispatch({
+      changes: {
+        from: 0,
+        to: this.view.state.doc.length,
+        insert: text,
+      },
+    })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,6 @@ graphComponent.inputMode = new GraphEditorInputMode()
 
 void graphComponent.fitGraphBounds()
 
-const editor = new EditorComponent()
+const editor = new EditorComponent(document.querySelector(".editor-container")!)
 
 editor.update({ shape: 'ellipse' })
